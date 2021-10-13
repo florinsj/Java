a50264772a455bf5bcd461b0ade3abada08124fd
Florin Stanciu <florinspad@gmail.com>
2021-10-13 23:52:24 +0300

    simple POST mapping springBoot maven - one parameter-->password

---
+++ Java_Exercises/E4_PostMappingControllerPassword.java
@@ -0,0 +1,15 @@
+package Java_Exercises;
+
+// @RestController
+// @RequestMapping("/passwords")
+public class E4_PostMappingControllerPassword {
+
+    public static void main(String[] args) {
+        System.out.println("Please uncomment and use in springboot- maven project ");
+    }
+//    @PostMapping(value = "/password")
+//    public void checkPassword(@RequestBody String password) {
+//        System.out.println(new PasswordEncrypt().encrypt(password));
+//    }
+
+}
